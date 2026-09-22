9dbef67a0cb38a21995a22eb41c1fe0e1e2657f1
bonum119 <nbartolomemarquez@gmail.com>
2026-09-22 09:30:48 +0200

    EjercicioGasolinera4

--- Ejercicio Gasolinera/src/ejercicio/gasolinera/ClienteRepositorio.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public interface ClienteRepositorio {
-    void guardar(Cliente cliente);
-    List obtenerTodos();
-    Cliente obtenerPorId(int id);
-    Cliente obtenerPorMatricula(String matricula);
-    int obtenerSiguienteId();
-}
--- Ejercicio Gasolinera/src/ejercicio/gasolinera/PagoRepositorio.java
+++
@@ -1,7 +0,0 @@
-import java.util.List;
-
-public interface PagoRepositorio {
-    void guardar(Pagos pago);
-    List obtenerTodos();
-    int obtenerSiguienteId();
-}
